a3ed58396108377872a00a9bc18adb3398694135
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2022-10-20 20:43:28 +0100

    Add an alt message to Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Gallery() {
     return (
         <div>
-            <img src='https://www.minecraft.net/content/dam/games/minecraft/key-art/Games_Subnav_Minecraft-300x465.jpg'/>
+            <img src='https://www.minecraft.net/content/dam/games/minecraft/key-art/Games_Subnav_Minecraft-300x465.jpg' alt="temp"/>
         </div>
     );
 }
