885b9589de03b936a79d4ee111ea3217d3f565a8
Andrew Alford <35538694+andrewalford1@users.noreply.github.com>
2022-10-20 20:33:00 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       
       <Navigation/>
 
+      <hr/>
       <Footer/>
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from '../Link/Link';
+
+const Footer = () => (
+    <div>
+        <Link 
+        href="https://andrewalford1.github.io/me/#/" 
+        text="Andrew Alford 2022" />
+    </div>
+);
+
+export default Footer;
